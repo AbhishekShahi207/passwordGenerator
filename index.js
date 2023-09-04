75d67d3a10d50106c2ad1f6c1aa98836ccf497a4
AbhishekShahi207 <shahiabhi1005@gmail.com>
2023-09-04 23:29:37 +0530

    seperating js and html

--- index.js
+++ index.js
@@ -1,6 +1,26 @@
-let Click=document.getElementsByClassName("btn")
+const passBox=document.getElementById("password");
+const lenght=8;
+const upperCase="ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+const lowerCase="abcdefghijklmnopqrstuvwxyz";
+const number="0123456789"
+const symbol="@!#$%&"
 
 
-function generate(){
-    alert("hello worl")
+const createPassword=()=>{
+    let password="";
+    password+=upperCase[Math.floor(Math.random() * upperCase.length)]
+    password+=lowerCase[Math.floor(Math.random() * lowerCase.length)]
+    password+=number[Math.floor(Math.random() * number.length)]
+    password+=symbol[Math.floor(Math.random() * symbol.length)]
+    password+=number[Math.floor(Math.random() * number.length)]
+    password+=lowerCase[Math.floor(Math.random() * lowerCase.length)]
+    password+=symbol[Math.floor(Math.random() * symbol.length)]
+    password+=upperCase[Math.floor(Math.random() * upperCase.length)]
+passBox.value=password;
+
+}
+const copy=()=>{
+    passBox.select();
+    navigator.clipboard.writeText(passBox.value)
+    alert("Text copied Successfully")
 }
